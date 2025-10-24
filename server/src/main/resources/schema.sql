d281a2b9b9ecfdcf1c0c6cb4fdb2fd35f91dbb70
Alex <ao11@my.yorku.ca>
2025-10-23 23:53:46 -0400

    Add database schema
    Adding a schema for the database, which is automatically executed by
    spring on startup. For this reason it is not necessary to mount volumes
    in docker compose anymore.

---
+++ server/src/main/resources/schema.sql
@@ -0,0 +1,33 @@
+-- Products
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(60) NOT NULL,
+    description TEXT,
+    price DECIMAL(5, 2) UNSIGNED NOT NULL,
+    category ENUM(
+        'CPU',
+        'GPU',
+        'PSU',
+        'MEMORY',
+        'STORAGE',
+        'MOTHERBOARD',
+        'COOLER',
+        'CASE',
+        'PERIPHERAL',
+    ),
+    quantity INT UNSIGNED NOT NULL DEFAULT 0,
+    brand_id INT REFERENCES brands(id) ON DELETE CASCADE
+);
+
+CREATE TABLE brands (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    description TEXT
+);
+
+CREATE TABLE images (
+    id SERIAL PRIMARY KEY,
+    product_id INT NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+    url TEXT NOT NULL,
+    alt_text VARCHAR(255)
+);
