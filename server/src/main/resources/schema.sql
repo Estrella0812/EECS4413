990ccc389df191a93497738390e42701c3a94cbe
Alex <ao11@my.yorku.ca>
2025-11-03 23:41:35 -0500

    Fix various sql errors

--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -1,31 +1,25 @@
 -- Products
+DROP TABLE IF EXISTS brands;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS images;
+
+CREATE TABLE brands (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE
+);
+
 CREATE TABLE products (
-    id SERIAL PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(60) NOT NULL,
     description JSON,
     price DECIMAL(5, 2) UNSIGNED NOT NULL,
-    category ENUM(
-        'CPU',
-        'GPU',
-        'PSU',
-        'MEMORY',
-        'STORAGE',
-        'MOTHERBOARD',
-        'COOLER',
-        'CASE',
-        'PERIPHERAL',
-    ),
+    category ENUM('CPU', 'GPU', 'PSU', 'MEMORY', 'STORAGE', 'MOTHERBOARD', 'COOLER', 'CASE', 'PERIPHERAL') NOT NULL,
     quantity INT UNSIGNED NOT NULL DEFAULT 0,
     brand_id INT REFERENCES brands(id) ON DELETE CASCADE
 );
 
-CREATE TABLE brands (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL UNIQUE,
-);
-
 CREATE TABLE images (
-    id SERIAL PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     is_main BIT(1),
     url TEXT NOT NULL,
     alt_text VARCHAR(255),
