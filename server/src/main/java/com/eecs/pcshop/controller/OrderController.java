e75437bf90fd391d280e24677493a88cff00949f
Alex <ao11@my.yorku.ca>
2025-12-19 04:43:33 -0500

    Add admin page routes

--- server/src/main/java/com/eecs/pcshop/controller/OrderController.java
+++ server/src/main/java/com/eecs/pcshop/controller/OrderController.java
@@ -2,7 +2,13 @@ package com.eecs.pcshop.controller;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,5 +34,12 @@ public class OrderController {
                 orderRepository.findByUserOrderByCreatedAtDesc(user);
 
         return ResponseEntity.ok(orders);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/all")
+    public ResponseEntity<PagedModel<Order>> getAllOrders(
+            @PageableDefault(size = 20, sort = "orderDate", direction = Sort.Direction.DESC) Pageable pageable) {
+        return ResponseEntity.ok(new PagedModel<>(orderRepository.findAll(pageable)));
     }
 }
